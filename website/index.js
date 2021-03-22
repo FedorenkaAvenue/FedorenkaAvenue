78bdba610d0d07d04297f9a8635b77c31046e3d5
bringmetheaugust <bringmetheaugust@gmail.com>
2021-03-22 10:07:04 +0200

    test website boards

--- website/index.js
+++ website/index.js
@@ -6,12 +6,13 @@ window.addEventListener('load', getDockerStatus);
 
 async function getDockerStatus() {
     try {
-        const res = await fetch('http://localhost:1991/dockerps');
+        const res = await fetch('/docker/ps');
         const responce = await res.text();
+
+        if (res.status === 406) return dockerTestServerBoard.innerHTML = 'docker error :(';
 
         dockerTestServerBoard.innerHTML = responce;
     } catch(err) {
         dockerTestServerBoard.innerHTML = REQUEST_ERROR;
-        console.error(err);
     }
 }
